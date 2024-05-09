3ded710856a8a7ab4125705aa284fe09775d4356
mohamad.shoaib <145815369+m23e36@users.noreply.github.com>
2024-05-09 08:46:59 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 6000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 6000);   
+}
+
+
+    let toggleBtn = document.querySelector("#toggleBtn");
+
+      let bodyEl = document.querySelector("body");
+
+      let darkMode = false;
+
+      toggleBtn.addEventListener('change', (event) => {
+        darkMode = event.target.checked;
+        if(darkMode) {
+          bodyEl.classList.add("dark");
+        } else {
+          bodyEl.classList.remove("dark");
+        }
+      })
+
+
+
+      
+
+      
+
+
---
+++ code/scripts.js
@@ -0,0 +1,7 @@
+var aimation = bodymovin.loadAnimation({
+    container:document.getElementById('bm'),
+     rederer:'svg',
+     loop:true,
+     autoplay: true,
+     path: 'code/data.json'
+})
